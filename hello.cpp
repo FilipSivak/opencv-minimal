2c6eef49bfa5e9206fbab20814f48d9ef46f2dff
Filip Sivak <sivakfil@gmail.com>
2020-04-21 06:48:16 +0000

    Colored image

--- hello.cpp
+++ hello.cpp
@@ -6,13 +6,23 @@
 int main() {
     
     unsigned char img_data[] = {
-        0, 255, 0, 255, 
-        255, 0, 255, 0
+	// first row, 4 colored pixels
+	0, 0, 0,
+	255, 0, 0,
+	0, 0, 0 ,
+	0, 255, 0,
+
+	// second row, 4 colored pixels
+	0, 0, 255,
+	0, 0, 0,
+	255, 0, 255,
+	0, 0, 0
     };
 
     unsigned char * img_data_ptr = (unsigned char*) &img_data;
 
-    cv::Mat img(2, 4, CV_8UC1, img_data_ptr);
+    cv::Mat img(2, 4, CV_8UC3, img_data_ptr);
+    cv::cvtColor(img, img, cv::COLOR_BGR2RGB);
     cv::imwrite("/out/myimage.png", img);
 
     std::cout << "Image written!\n";
